0288cf7e45495c2023a20d5de61222c9f43ba045
daothan <daothan12112@gmail.com>
2017-09-28 08:27:07 +0700

    add NODE

--- API-Serverless/handler.js
+++ API-Serverless/handler.js
@@ -1,14 +1,15 @@
-'use strict';
+
 
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
 
-  if(typeof data.text !== 'string'){
+  if (typeof data.text !== 'string') {
     console.error('Validation failed');
     callback(new Error('Couldnt create item'));
     return;
@@ -20,14 +21,14 @@ module.exports.create = (event, context, callback) => {
       text: data.text,
       checked: false,
       createdAt: timestamp,
-      updateAt: timestamp
-    }
-  }
- 
+      updateAt: timestamp,
+    },
+  };
+
 
   dynamoDb.put(params, (error, result) => {
     console.log(params);
-    if(error) {
+    if (error) {
       console.error(error);
       callback(new Error('Could not create books item'));
       return;
@@ -35,27 +36,106 @@ module.exports.create = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item)
-    }
+      body: JSON.stringify(result.Item),
+    };
     callback(null, response);
-  })
-}
+  });
+};
 
 module.exports.list = (event, content, callback) => {
   const params = {
-    TableName: 'Books'
+    TableName: 'Books',
   };
   dynamoDb.scan(params, (error, result) => {
-    if(error) {
+    if (error) {
       console.error(error);
       callback(new Error('Could not fetch the data'));
       return;
     }
-    
+
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Items)
+      body: JSON.stringify(result.Items),
     };
     callback(null, response);
   });
-}
+};
+
+
+module.exports.listById = (event, content, callback) => {
+  const params = {
+    TableName: 'Books',
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+  dynamoDb.get(params, (error, result) => {
+    console.log(params);
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not found'));
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.update = (event, content, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  console.log(data);
+  if (typeof data.text !== 'string' || typeof data.checked !== 'boolean') {
+    console.error('Validation error');
+    callback(new Error('Couldnt update the item'));
+    return;
+  }
+
+  const params = {
+    TableName: 'Books',
+    Item: {
+      id: event.pathParameters.id,
+      text: data.text,
+      checked: data.checked,
+      updatedAt: timestamp,
+    },
+  };
+
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not update'));
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.delete = (event, content, callback) => {
+  const params = {
+    TableName: 'Books',
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.delete(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not delete this item'));
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
